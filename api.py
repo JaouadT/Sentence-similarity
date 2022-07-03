3abe68cc37e16bd77c176dcbe57fb925be6e7d4b
Jaouad Tagnamas <jaouad@Jaouads-MacBook-Pro.local>
2022-07-03 19:28:03 +0100

    commit

--- api.py
+++ api.py
@@ -67,5 +67,5 @@ def get_similarity_multiple_sentences():
 
     return Response(response=json.dumps({"response": result}), mimetype="application/json")
     
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8000)
+# if __name__ == '__main__':
+#     app.run(debug=True, host='0.0.0.0', port=8000)
