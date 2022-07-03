303c2d7e207107f47eb3b6ae42a7c75eaa41634d
Jaouad Tagnamas <jaouad@Jaouads-MacBook-Pro.local>
2022-07-03 22:12:10 +0100

    commit1

--- app.py
+++ app.py
@@ -70,5 +70,3 @@ def get_similarity_multiple_sentences():
 # if __name__ == '__main__':
 #     app.run(debug=True, host='0.0.0.0', port=8000)
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
